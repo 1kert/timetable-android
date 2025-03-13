f2cdfe6f3e3e55b7b1dc54e6d2ac6cb5c4692255
Kert Mänd <kertmand1@gmail.com>
2025-03-13 16:07:35 +0200

    added TimetableEvent for preview; started on HorizontalPager for EventScreen

--- app/src/main/java/com/example/timetable/ui/screens/EventScreen.kt
+++ app/src/main/java/com/example/timetable/ui/screens/EventScreen.kt
@@ -1,6 +1,8 @@
 package com.example.timetable.ui.screens
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
@@ -10,14 +12,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.timetable.components.EventCard
 import com.example.timetable.data.TimetableEvent
 import com.example.timetable.ui.theme.TimetableTheme
 
 @Composable
 fun EventScreen(
     title: String,
-    events: List<List<TimetableEvent>>
+    events: List<List<TimetableEvent>>,
+    getDateString: (events: List<TimetableEvent>) -> String
 ) {
     var dateString by remember { mutableStateOf("some date string") } // todo: should be empty
 
@@ -39,20 +45,41 @@ fun EventScreen(
                 }
 
                 Column(
+                    horizontalAlignment = Alignment.End,
 
                 ) {
                     Text(
-                        text = dateString
+                        text = dateString,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 24.sp,
                     )
 
                     Text(
-                        text = title
+                        text = title,
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Bold
                     )
                 }
             }
         }
     ) { contentPadding ->
-        Box(Modifier.padding(contentPadding))
+        HorizontalPager(
+            state = rememberPagerState { events.size },
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(contentPadding)
+        ) { page ->
+            LaunchedEffect(Unit) {
+                dateString = getDateString(events[page])
+            }
+
+            Column {
+                val todayEvents = events[page]
+                todayEvents.forEach {
+                    EventCard(it)
+                }
+            }
+        }
     }
 }
 
@@ -61,8 +88,22 @@ fun EventScreen(
 private fun EventScreenPreview() {
     TimetableTheme {
         EventScreen(
-            title = "some test title",
-            events = listOf()
+            title = "K-209",
+            events = listOf(
+                listOf(
+                    TimetableEvent(
+                        name = "Mingi tund",
+                        date = "2024-12-17T00:00:00Z",
+                        timeStart = "22:00",
+                        timeEnd = "22:01",
+                        teachers = listOf(),
+                        rooms = listOf(),
+                        studentGroups = listOf(),
+                        singleEvent = false
+                    )
+                )
+            ),
+            getDateString = {_ -> "31. November"}
         )
     }
 }
